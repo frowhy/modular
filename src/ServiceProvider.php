ca370e547f734807b6afd3cc38fe658c15e363f9
Govern Fu <frowhy@gmail.com>
2019-05-20 14:05:45 +0800

    :sparkles: Publish asset add groups

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -10,23 +10,26 @@ class ServiceProvider extends LaravelServiceProvider
     public function boot()
     {
         $this->publishes([
-            dirname(__DIR__).'/stubs/modules.php' => config_path('modules.php'),
-        ]);
+            dirname(__DIR__).'/config/modules.php' => config_path('modules.php'),
+        ], 'config');
         $this->publishes([
-            dirname(__DIR__).'/stubs/repository.php' => config_path('repository.php'),
-        ]);
+            dirname(__DIR__).'/config/repository.php' => config_path('repository.php'),
+        ], 'config');
         $this->publishes([
             dirname(__DIR__).'/stubs/modules' => base_path('modules'),
-        ]);
+        ], 'initialize');
+        $this->publishes([
+            dirname(__DIR__).'/stubs/Example' => base_path('modules/Example'),
+        ], 'example');
     }
 
     public function register()
     {
         $this->mergeConfigFrom(
-            dirname(__DIR__).'/stubs/modules.php', 'modules'
+            dirname(__DIR__).'/config/modules.php', 'modules'
         );
         $this->mergeConfigFrom(
-            dirname(__DIR__).'/stubs/repository.php', 'repository'
+            dirname(__DIR__).'/config/repository.php', 'repository'
         );
     }
 }

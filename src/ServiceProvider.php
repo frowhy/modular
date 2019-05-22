c08f11ef05a6900e5d62acc7725832d8eb99087d
Govern Fu <frowhy@gmail.com>
2019-05-22 09:24:03 +0800

    :fire: Remove modules config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -13,18 +13,12 @@ class ServiceProvider extends LaravelServiceProvider
             dirname(__DIR__).'/config/modules.php' => config_path('modules.php'),
         ], 'modular-config');
         $this->publishes([
-            dirname(__DIR__).'/config/repository.php' => config_path('repository.php'),
-        ], 'modular-config');
-        $this->publishes([
             dirname(__DIR__).'/stubs/example' => base_path('modules/Example'),
         ], 'modular-example');
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(
-            dirname(__DIR__).'/config/modules.php', 'modules'
-        );
         $this->mergeConfigFrom(
             dirname(__DIR__).'/config/repository.php', 'repository'
         );

84158052a7e348cd3665ff637b485538ce5dee12
Govern Fu <frowhy@gmail.com>
2019-05-23 16:43:45 +0800

    :bug: Repair config publish

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -9,7 +9,7 @@ class ServiceProvider extends LaravelServiceProvider
     public function boot()
     {
         $this->publishes([
-            dirname(__DIR__).'/config/modules.php' => config_path('modules.php'),
+            dirname(__DIR__).'/config/repository.php' => config_path('repository.php'),
         ], 'modular-config');
         $this->publishes([
             dirname(__DIR__).'/stubs/example' => base_path('modules/Example'),
